df8b4cdd19248b2281e9ff73932bebad8dd4afdf
Martin <mwha@itu.dk>
2022-09-07 16:02:19 +0200

    Created skeleton for code

--- Program.go
+++ Program.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"time"
+)
+
+type Fork struct{ available bool }
+
 func main() {
+	forkOne := make(chan *Fork)
+	forkTwo := make(chan *Fork)
+	forkThree := make(chan *Fork)
+	forkFour := make(chan *Fork)
+	forkFive := make(chan *Fork)
+	go philosopher("p1", forkOne, forkTwo)
+
+	forkOne <- new(Fork)
+	forkTwo <- new(Fork)
+	forkThree <- new(Fork)
+	forkFour <- new(Fork)
+	forkFive <- new(Fork)
+	time.Sleep(5 * time.Second)
+	<-forkOne
+	<-forkTwo
+	<-forkThree
+	<-forkFour
+	<-forkFive
+}
+
+func philosopher(name string, forkLeft chan *Fork, forkRight chan *Fork) {
+	var timesEaten int = 0
+	for timesEaten < 3 {
+		think()
+		interactLeftFork()
+		interactRightFork()
+		eat(timesEaten)
+
+	}
+}
+
+func interactLeftFork() {
+
+}
+
+func interactRightFork() {
+
+}
+
+func eat(timesEaten int) {
+	timesEaten++
+	interactLeftFork()
+	interactRightFork()
+}
+
+func think() {
 
 }
