39d8e750720b9913c0d89083abd1a8cf719ded05
Lukas Eliassen <luel@itu.dk>
2022-09-14 11:40:37 +0200

    Create multiple channel solution

--- Dining.go
+++ Dining.go
@@ -23,10 +23,13 @@ var philosophers []Philosopher
 var forks []Fork
 
 // Function to create philolopher goroutines.
-func aPhilosopher(index int, leftFork int, rightFork int, ch chan bool) {
+func aPhilosopher(index int, leftFork int, rightFork int, chL chan bool, chR chan bool) {
 	// Acces the specific philosopher in the philosophers slice.
 	philosopher := &philosophers[index]
 
+	for true{
+		
+	
 	// If the philosopher has eaten 3 times it set philosopher.doneEating to true
 	if philosopher.timesEaten == 3 {
 		philosopher.doneEating = true
@@ -36,49 +39,55 @@ func aPhilosopher(index int, leftFork int, rightFork int, ch chan bool) {
 	// Logic to control when the philosopher should eat or think.
 	if !philosopher.doneEating {
 
-		fmt.Println(philosopher.name + " thinking")
-
 		if !forks[leftFork].forkIsActive {
 			philosopher.useLeftFork = true
-			ch <- true
+			chL <- true
 		}
 
 		if !forks[rightFork].forkIsActive {
 			philosopher.useRightFork = true
-			ch <- true
+			chR <- true
 		}
 
 		if philosopher.useLeftFork == true && philosopher.useRightFork == true {
 			philosopher.eat = true
 			fmt.Println(philosopher.name + " eating")
 			philosopher.timesEaten++
+			fmt.Println(philosopher.name+" has eaten", philosopher.timesEaten, "times")
 		}
-
 		if philosopher.eat && philosopher.useLeftFork == true {
 			philosopher.useLeftFork = false
-			ch <- false
+			chL <- false
 		}
-
+		
 		if philosopher.eat && philosopher.useRightFork == true {
 			philosopher.useRightFork = false
-			ch <- false
+			chR <- false
 		}
-
-		if philosopher.eat && philosopher.useLeftFork == true && philosopher.useRightFork == true {
+		if(philosopher.eat){
 			philosopher.eat = false
 			fmt.Println(philosopher.name + " thinking")
 		}
+		
 	}
+	if(philosopher.doneEating){
+		break;
+	}
+}
 }
 
 // Function to create fork goroutines.
 func aFork(index int, ch chan bool) {
 	// Acces the specific fork in the forks slice.
 	fork := &forks[index]
+	for(true){
+		boolVal := <-ch
+		fork.forkIsActive = boolVal
+	}
+	
 
 	// Channel from aPhilosopher which tells aFork if its active.
-	boolVal := <-ch
-	fork.forkIsActive = boolVal
+	
 }
 
 func main() {
@@ -108,38 +117,28 @@ func main() {
 	forks = append(forks, Fork{forkIsActive: false})
 	forks = append(forks, Fork{forkIsActive: false})
 
-	ch := make(chan bool)
+	ch0 := make(chan bool)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	ch3 := make(chan bool)
+	ch4 := make(chan bool)
+	
+		go aPhilosopher(0, 4, 0, ch4, ch0)
+		go aPhilosopher(1, 0, 1, ch0, ch1)
+		go aPhilosopher(2, 2, 1, ch2, ch1)
+		go aPhilosopher(3, 2, 3, ch2, ch3)
+		go aPhilosopher(4, 3, 4, ch3, ch4)
+		
+		go aFork(0, ch0)
+		go aFork(1, ch1)
+		go aFork(2, ch2)
+		go aFork(3, ch3)
+		go aFork(4, ch4)
 
-	// For-loop runs while the philosophers is not done eating.
-	for !philosophers[0].doneEating && !philosophers[1].doneEating && !philosophers[2].doneEating && !philosophers[3].doneEating && !philosophers[4].doneEating {
-		if !philosophers[0].doneEating {
-			go aPhilosopher(0, 4, 0, ch)
-		}
-		if !philosophers[1].doneEating {
-			go aPhilosopher(1, 0, 1, ch)
-		}
-		if !philosophers[2].doneEating {
-			go aPhilosopher(2, 1, 2, ch)
-		}
-		if !philosophers[3].doneEating {
-			go aPhilosopher(3, 2, 3, ch)
-		}
-		if !philosophers[4].doneEating {
-			go aPhilosopher(4, 3, 4, ch)
-		}
-
-		go aFork(0, ch)
-		go aFork(1, ch)
-		go aFork(2, ch)
-		go aFork(3, ch)
-		go aFork(4, ch)
-
-		time.Sleep(time.Millisecond)
-	}
-
-	fmt.Println(philosophers[0].name+" has eaten", philosophers[0].timesEaten, "times")
-	fmt.Println(philosophers[1].name+" has eaten", philosophers[1].timesEaten, "times")
+	//fmt.Println(philosophers[0].name+" has eaten", philosophers[0].timesEaten, "times")
+	/*fmt.Println(philosophers[1].name+" has eaten", philosophers[1].timesEaten, "times")
 	fmt.Println(philosophers[2].name+" has eaten", philosophers[2].timesEaten, "times")
 	fmt.Println(philosophers[3].name+" has eaten", philosophers[3].timesEaten, "times")
-	fmt.Println(philosophers[4].name+" has eaten", philosophers[4].timesEaten, "times")
+	fmt.Println(philosophers[4].name+" has eaten", philosophers[4].timesEaten, "times")*/
+	time.Sleep(1000*time.Millisecond)
 }
